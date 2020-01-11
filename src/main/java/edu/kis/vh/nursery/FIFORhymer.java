84737c72b46de979fbd9e73a4ada2ef907ea8c16
Krzysztof Karasiński <krzysiek1105@opoczta.pl>
2020-01-11 14:47:10 +0100

    1.4 Accessory i hermetyzacja

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery;
 
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+    private DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
     @Override
     public int countOut() {
@@ -18,4 +18,5 @@ public class FIFORhymer extends DefaultCountingOutRhymer {
 
         return ret;
     }
+
 }
