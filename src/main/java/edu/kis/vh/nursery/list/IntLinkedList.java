654a2a3a73843e583cc2398ceba80015d16a2775
Krzysztof Karasiński <krzysiek1105@opoczta.pl>
2020-01-12 21:22:16 +0100

    1.5 Ekstrakcja kodu do metody, javadoc klasy IntLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,9 +1,16 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * Basic linked list implementation. It can only contain integers.
+ */
 public class IntLinkedList {
     public static final int LIST_IS_EMPTY = -1;
     private Node last;
 
+    /***
+     * Add element to the linked list
+     * @param i element
+     */
     public void push(int i) {
         if (last == null)
             last = new Node(i);
@@ -14,20 +21,37 @@ public class IntLinkedList {
         }
     }
 
+    /***
+     * Check if list is empty
+     * @return true if empty
+     */
+
     public boolean isEmpty() {
         return last == null;
     }
 
+    /***
+     * Linked list cannot be full, so it is always false value
+     * @return false
+     */
     public boolean isFull() {
         return false;
     }
 
+    /***
+     * Take the last item from the list without deleting it
+     * @return top element
+     */
     public int top() {
         if (isEmpty())
             return LIST_IS_EMPTY;
         return last.getValue();
     }
 
+    /***
+     * Take the last item from the list and delete it
+     * @return top element
+     */
     public int pop() {
         if (isEmpty())
             return LIST_IS_EMPTY;
